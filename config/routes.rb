4b3432d069c274884765e058a459351da3a4c409
Pedro Maciel <pedrozath@gmail.com>
2016-04-11 00:13:00 -0300

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
 
     get "/check_out" => "orders#check_out"
     get "/payment/notification" => "orders#notification"
-    get "/payment/redirect" => "orders#redirect"
-    post "/payment/redirect" => "orders#redirect"
+    get "/payment/redirect" => "pages#compra_concluida"
+    post "/payment/redirect" => "pages#compra_concluida"
 
     resources :products
     resources :images
