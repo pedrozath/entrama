a4c3f2ad12e415d18298b1d6d507cc8621b5282b
Pedro Maciel <pedrozath@gmail.com>
2016-05-02 09:43:40 -0300

    updates

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,4 +7,16 @@ class ImageUploader < CarrierWave::Uploader::Base
         process resize_to_fit: [100,100]
     end
 
+    version :small do
+        process resize_to_fit: [200,200]
+    end
+
+    version :medium do
+        process resize_to_fit: [500,500]
+    end
+
+    version :big do
+        process resize_to_fit: [800,800]
+    end
+
 end
